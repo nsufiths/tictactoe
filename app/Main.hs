bf6c6b08b894cadadfdf9995f0877728e10cac88
Kutalev Andrey <a.kutalev@g.nsu.ru>
2022-03-13 19:56:47 +0700

    player class type

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,5 @@
 module Main where
-import System.Random
 import Lib
-
-getTurnFirst::IO (Int, Int)
-getTurnFirst = do 
-    x <- randomRIO (0,3)
-    y <- randomRIO (0,3)
-    return (x,y)
-
-getTurnSecond::IO (Int, Int)
-getTurnSecond = do 
-    x <- randomRIO (0,3)
-    y <- randomRIO (0,3)
-    return (x,y)
 
 main :: IO ()
 main = print $ play
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 module Lib  where
 import Control.Monad.State
 
-data GameState =  GameState {field :: [[Mark]], currentPlayer :: Player } deriving (Eq, Show)
+data GameState =  GameState {field :: [[Mark]], currentPlayer :: Player} deriving (Eq, Show)
 data Player    = XPlayer | OPlayer deriving (Eq, Show)
 data Mark      = XMark | OMark | Empty deriving (Eq, Show)
 data Winner    = None | Draw | Winner Player deriving (Eq, Show)
@@ -10,7 +10,7 @@ initialState = GameState (replicate 3 $ replicate 3 Empty) XPlayer
 
 getCoords::GameState->(Int, Int)
 getCoords (GameState f XPlayer) =  if f !! 0 !! 0 == Empty then (0,0) else if  f !! 0 !! 1 == Empty then (1,0) else (2,0)
-getCoords (GameState _ OPlayer) = (0,1)
+getCoords (GameState f OPlayer) =  if f !! 1 !! 0 == Empty then (0,1) else if  f !! 1 !! 1 == Empty then (1,1) else (2,1)
 
 gamePlay:: State GameState Winner
 gamePlay = do 
@@ -19,9 +19,9 @@ gamePlay = do
     if winner /= None then return winner else do
         let coords = getCoords st
         putMark coords
-        let newPayer = if currentPlayer st == XPlayer then OPlayer else XPlayer
+        let newPlayer = if currentPlayer st == XPlayer then OPlayer else XPlayer
         st <- get
-        put (st {currentPlayer = newPayer})
+        put (st {currentPlayer = newPlayer})
         gamePlay
 
 
